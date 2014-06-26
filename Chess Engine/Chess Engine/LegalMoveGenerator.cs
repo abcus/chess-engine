94a3efccc98c887586bb0f0d26faa3726581a665
abcus <darkmatter_9@hotmail.com>
2014-06-26 19:36:37 -0400

    FEN parsing and find first set working

--- Chess Engine/Chess Engine/LegalMoveGenerator.cs
+++ Chess Engine/Chess Engine/LegalMoveGenerator.cs
@@ -8,20 +8,7 @@ namespace Chess_Engine {
 
     class LegalMoveGenerator {
 
-        private static int ffs(long input) {
-
-            if (input == 0) {
-                return 0;
-            }
-            long mask = 1;
-            int index = 0;
-
-            while ((input & mask) == 0) {
-                mask = mask << 1;
-                index = index + 1;
-            }
-            return index;
-        }
+      
 
     }
 }
--- Chess Engine/Chess Engine/Program.cs
+++ Chess Engine/Chess Engine/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,14 @@ namespace Chess_Engine {
     class Program {
         
         static void Main(string[] args) {
-  
+
+
+
+
+
+            Console.WriteLine(Constants.findFirstSet(585748335649226752));
+
+
             //Creates a new engine object and calls its run method
             Engine chessEngine = new Engine();
             chessEngine.run();
